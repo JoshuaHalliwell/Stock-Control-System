5c0aebad1d031a51d9002d461ff63837d8990aa1
Jack Cartwright <JackN8345@student.sjr.ac.uk>
2023-03-02 15:08:28 +0000

    StaffAdd

---
+++ Stock Control System/StaffAdd.vb
@@ -0,0 +1,21 @@
+﻿Public Class StaffAdd
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs) Handles TextBox7.TextChanged
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles Label10.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
