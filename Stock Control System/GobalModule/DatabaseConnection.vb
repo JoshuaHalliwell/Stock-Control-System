a0f5a88dda7c460aff7882def4be7e7569a7f6b2
thomasn8452 <thomasn8452@student.sjr.ac.uk>
2023-03-08 15:57:41 +0000

    Y

--- Stock Control System/GobalModule/DatabaseConnection.vb
+++ Stock Control System/GobalModule/DatabaseConnection.vb
@@ -2,7 +2,7 @@
     Public MaxRows As Integer 'The maxuim amount rows of the table'
     Public MaxColumns As Integer 'The maxuim amount columns of the table'
     Public curRow As Integer 'The current row being inseted into the table'
-    Public con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & Application.StartupPath & "/StockDataBase.accdb") 'Useing a pre- defined Library to connect to the database'
+    Public con As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & Environment.ExpandEnvironmentVariables("%USERPROFILE%") & "\source\repos\Stock-Control-System\StockDataBase.accdb") 'Useing a pre- defined Library to connect to the database'   & Application.StartupPath &
     Public ds As New DataSet 'A table from microsoft access is now refred to as DataSet'
     Public da As OleDb.OleDbDataAdapter 'Used to push data in and out data betweent applictions such as Micrsoft Access and Visual Students   
     Public sql As String 'The current SQL command as a string' 
