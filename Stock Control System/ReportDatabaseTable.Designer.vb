61cecbc6efe1af9b7c7cd269fedf5ee67b8787ff
DoddTh04 <thomasn8452@student.sjr.ac.uk>
2023-03-09 00:03:02 +0000

    Report change

--- Stock Control System/ReportDatabaseTable.Designer.vb
+++ Stock Control System/ReportDatabaseTable.Designer.vb
@@ -22,30 +22,122 @@ Partial Class ReportDatabaseTable
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.CbxTableSelection = New System.Windows.Forms.ComboBox()
+        Me.ClbGraphData = New System.Windows.Forms.CheckedListBox()
+        Me.LblGraphData = New System.Windows.Forms.Label()
+        Me.BtnClearTable = New System.Windows.Forms.Button()
+        Me.BtnBackupTable = New System.Windows.Forms.Button()
+        Me.BtnShareByEmail = New System.Windows.Forms.Button()
+        Me.BtnBackToMenu = New System.Windows.Forms.Button()
+        Me.BtnSaveToPDF = New System.Windows.Forms.Button()
+        Me.DGVDataTable = New System.Windows.Forms.DataGridView()
+        CType(Me.DGVDataTable, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'CbxTableSelection
+        'ClbGraphData
         '
-        Me.CbxTableSelection.FormattingEnabled = True
-        Me.CbxTableSelection.Items.AddRange(New Object() {"Customer Tabel", "Orders Table", "Sales Table", "SaleLine Table", "Staff Table ", "Stocks Table ", "Suppliers Table "})
-        Me.CbxTableSelection.Location = New System.Drawing.Point(154, 64)
-        Me.CbxTableSelection.Name = "CbxTableSelection"
-        Me.CbxTableSelection.Size = New System.Drawing.Size(121, 21)
-        Me.CbxTableSelection.TabIndex = 0
+        Me.ClbGraphData.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClbGraphData.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClbGraphData.FormattingEnabled = True
+        Me.ClbGraphData.Items.AddRange(New Object() {"Staff Table  ", "Customer Table ", "Suppliers Orders Table", "Sales Table", "Stocks Table", "Suppliers Table"})
+        Me.ClbGraphData.Location = New System.Drawing.Point(590, 84)
+        Me.ClbGraphData.Name = "ClbGraphData"
+        Me.ClbGraphData.Size = New System.Drawing.Size(257, 179)
+        Me.ClbGraphData.TabIndex = 3
+        '
+        'LblGraphData
+        '
+        Me.LblGraphData.AutoSize = True
+        Me.LblGraphData.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblGraphData.Location = New System.Drawing.Point(622, 24)
+        Me.LblGraphData.Name = "LblGraphData"
+        Me.LblGraphData.Size = New System.Drawing.Size(140, 29)
+        Me.LblGraphData.TabIndex = 9
+        Me.LblGraphData.Text = "Graph Data:"
+        '
+        'BtnClearTable
+        '
+        Me.BtnClearTable.Location = New System.Drawing.Point(12, 326)
+        Me.BtnClearTable.Name = "BtnClearTable"
+        Me.BtnClearTable.Size = New System.Drawing.Size(132, 81)
+        Me.BtnClearTable.TabIndex = 10
+        Me.BtnClearTable.Text = "Clear Table View"
+        Me.BtnClearTable.UseVisualStyleBackColor = True
+        '
+        'BtnBackupTable
+        '
+        Me.BtnBackupTable.Location = New System.Drawing.Point(188, 326)
+        Me.BtnBackupTable.Name = "BtnBackupTable"
+        Me.BtnBackupTable.Size = New System.Drawing.Size(132, 81)
+        Me.BtnBackupTable.TabIndex = 11
+        Me.BtnBackupTable.Text = "Backup Data To Excel"
+        Me.BtnBackupTable.UseVisualStyleBackColor = True
+        '
+        'BtnShareByEmail
+        '
+        Me.BtnShareByEmail.Location = New System.Drawing.Point(361, 326)
+        Me.BtnShareByEmail.Name = "BtnShareByEmail"
+        Me.BtnShareByEmail.Size = New System.Drawing.Size(132, 81)
+        Me.BtnShareByEmail.TabIndex = 12
+        Me.BtnShareByEmail.Text = "Share data table vial Email"
+        Me.BtnShareByEmail.UseVisualStyleBackColor = True
+        '
+        'BtnBackToMenu
+        '
+        Me.BtnBackToMenu.Location = New System.Drawing.Point(715, 326)
+        Me.BtnBackToMenu.Name = "BtnBackToMenu"
+        Me.BtnBackToMenu.Size = New System.Drawing.Size(132, 81)
+        Me.BtnBackToMenu.TabIndex = 13
+        Me.BtnBackToMenu.Text = "Back To Report Menu"
+        Me.BtnBackToMenu.UseVisualStyleBackColor = True
+        '
+        'BtnSaveToPDF
+        '
+        Me.BtnSaveToPDF.Location = New System.Drawing.Point(537, 326)
+        Me.BtnSaveToPDF.Name = "BtnSaveToPDF"
+        Me.BtnSaveToPDF.Size = New System.Drawing.Size(132, 81)
+        Me.BtnSaveToPDF.TabIndex = 14
+        Me.BtnSaveToPDF.Text = "Save Coby Of Data To PDF"
+        Me.BtnSaveToPDF.UseVisualStyleBackColor = True
+        '
+        'DGVDataTable
+        '
+        Me.DGVDataTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGVDataTable.Location = New System.Drawing.Point(12, 24)
+        Me.DGVDataTable.Name = "DGVDataTable"
+        Me.DGVDataTable.RowHeadersWidth = 51
+        Me.DGVDataTable.RowTemplate.Height = 24
+        Me.DGVDataTable.Size = New System.Drawing.Size(567, 268)
+        Me.DGVDataTable.TabIndex = 15
         '
         'ReportDatabaseTable
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(669, 348)
-        Me.Controls.Add(Me.CbxTableSelection)
+        Me.ClientSize = New System.Drawing.Size(892, 428)
+        Me.Controls.Add(Me.DGVDataTable)
+        Me.Controls.Add(Me.BtnSaveToPDF)
+        Me.Controls.Add(Me.BtnBackToMenu)
+        Me.Controls.Add(Me.BtnShareByEmail)
+        Me.Controls.Add(Me.BtnBackupTable)
+        Me.Controls.Add(Me.BtnClearTable)
+        Me.Controls.Add(Me.LblGraphData)
+        Me.Controls.Add(Me.ClbGraphData)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "ReportDatabaseTable"
         Me.Text = "DatabaseTableReport"
+        CType(Me.DGVDataTable, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents CbxTableSelection As ComboBox
+    Friend WithEvents ClbGraphData As CheckedListBox
+    Friend WithEvents LblGraphData As Label
+    Friend WithEvents BtnClearTable As Button
+    Friend WithEvents BtnBackupTable As Button
+    Friend WithEvents BtnShareByEmail As Button
+    Friend WithEvents BtnBackToMenu As Button
+    Friend WithEvents BtnSaveToPDF As Button
+    Friend WithEvents DGVDataTable As DataGridView
 End Class
--- Stock Control System/ReportGraph.Designer.vb
+++ Stock Control System/ReportGraph.Designer.vb
@@ -22,30 +22,119 @@ Partial Class ReportGraph
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.CbxTableSelection = New System.Windows.Forms.ComboBox()
+        Me.ClbGraphData = New System.Windows.Forms.CheckedListBox()
+        Me.CbxGraphType = New System.Windows.Forms.ComboBox()
+        Me.BtnClearGraph = New System.Windows.Forms.Button()
+        Me.BtnEmailShare = New System.Windows.Forms.Button()
+        Me.BtnPdfShare = New System.Windows.Forms.Button()
+        Me.LblGraphData = New System.Windows.Forms.Label()
+        Me.LblGraphType = New System.Windows.Forms.Label()
+        Me.BtnBackToMenu = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'CbxTableSelection
+        'ClbGraphData
         '
-        Me.CbxTableSelection.FormattingEnabled = True
-        Me.CbxTableSelection.Items.AddRange(New Object() {"Customer Tabel", "Orders Table", "Sales Table", "SaleLine Table", "Staff Table ", "Stocks Table ", "Suppliers Table "})
-        Me.CbxTableSelection.Location = New System.Drawing.Point(419, 54)
-        Me.CbxTableSelection.Name = "CbxTableSelection"
-        Me.CbxTableSelection.Size = New System.Drawing.Size(121, 21)
-        Me.CbxTableSelection.TabIndex = 1
+        Me.ClbGraphData.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClbGraphData.FormattingEnabled = True
+        Me.ClbGraphData.Items.AddRange(New Object() {"Product sales for the shop ", "Over all stock", "Active Suppliers "})
+        Me.ClbGraphData.Location = New System.Drawing.Point(602, 61)
+        Me.ClbGraphData.Name = "ClbGraphData"
+        Me.ClbGraphData.Size = New System.Drawing.Size(257, 79)
+        Me.ClbGraphData.TabIndex = 2
+        '
+        'CbxGraphType
+        '
+        Me.CbxGraphType.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CbxGraphType.FormattingEnabled = True
+        Me.CbxGraphType.Items.AddRange(New Object() {"Pie Chart"})
+        Me.CbxGraphType.Location = New System.Drawing.Point(602, 207)
+        Me.CbxGraphType.Name = "CbxGraphType"
+        Me.CbxGraphType.Size = New System.Drawing.Size(257, 33)
+        Me.CbxGraphType.TabIndex = 4
+        '
+        'BtnClearGraph
+        '
+        Me.BtnClearGraph.Location = New System.Drawing.Point(12, 336)
+        Me.BtnClearGraph.Name = "BtnClearGraph"
+        Me.BtnClearGraph.Size = New System.Drawing.Size(172, 81)
+        Me.BtnClearGraph.TabIndex = 5
+        Me.BtnClearGraph.Text = "Clear Graph"
+        Me.BtnClearGraph.UseVisualStyleBackColor = True
+        '
+        'BtnEmailShare
+        '
+        Me.BtnEmailShare.Location = New System.Drawing.Point(245, 336)
+        Me.BtnEmailShare.Name = "BtnEmailShare"
+        Me.BtnEmailShare.Size = New System.Drawing.Size(172, 81)
+        Me.BtnEmailShare.TabIndex = 6
+        Me.BtnEmailShare.Text = "Share graph to email"
+        Me.BtnEmailShare.UseVisualStyleBackColor = True
+        '
+        'BtnPdfShare
+        '
+        Me.BtnPdfShare.Location = New System.Drawing.Point(478, 336)
+        Me.BtnPdfShare.Name = "BtnPdfShare"
+        Me.BtnPdfShare.Size = New System.Drawing.Size(172, 81)
+        Me.BtnPdfShare.TabIndex = 7
+        Me.BtnPdfShare.Text = "Save graph as PDF"
+        Me.BtnPdfShare.UseVisualStyleBackColor = True
+        '
+        'LblGraphData
+        '
+        Me.LblGraphData.AutoSize = True
+        Me.LblGraphData.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblGraphData.Location = New System.Drawing.Point(619, 18)
+        Me.LblGraphData.Name = "LblGraphData"
+        Me.LblGraphData.Size = New System.Drawing.Size(140, 29)
+        Me.LblGraphData.TabIndex = 8
+        Me.LblGraphData.Text = "Graph Data:"
+        '
+        'LblGraphType
+        '
+        Me.LblGraphType.AutoSize = True
+        Me.LblGraphType.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblGraphType.Location = New System.Drawing.Point(619, 175)
+        Me.LblGraphType.Name = "LblGraphType"
+        Me.LblGraphType.Size = New System.Drawing.Size(146, 29)
+        Me.LblGraphType.TabIndex = 9
+        Me.LblGraphType.Text = "Graph Type:"
+        '
+        'BtnBackToMenu
+        '
+        Me.BtnBackToMenu.Location = New System.Drawing.Point(712, 336)
+        Me.BtnBackToMenu.Name = "BtnBackToMenu"
+        Me.BtnBackToMenu.Size = New System.Drawing.Size(172, 81)
+        Me.BtnBackToMenu.TabIndex = 10
+        Me.BtnBackToMenu.Text = "Back to graph menu"
+        Me.BtnBackToMenu.UseVisualStyleBackColor = True
         '
         'ReportGraph
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(669, 348)
-        Me.Controls.Add(Me.CbxTableSelection)
+        Me.ClientSize = New System.Drawing.Size(896, 429)
+        Me.Controls.Add(Me.BtnBackToMenu)
+        Me.Controls.Add(Me.LblGraphType)
+        Me.Controls.Add(Me.LblGraphData)
+        Me.Controls.Add(Me.BtnPdfShare)
+        Me.Controls.Add(Me.BtnEmailShare)
+        Me.Controls.Add(Me.BtnClearGraph)
+        Me.Controls.Add(Me.CbxGraphType)
+        Me.Controls.Add(Me.ClbGraphData)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "ReportGraph"
         Me.Text = "GraphReport"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents CbxTableSelection As ComboBox
+    Friend WithEvents ClbGraphData As CheckedListBox
+    Friend WithEvents CbxGraphType As ComboBox
+    Friend WithEvents BtnClearGraph As Button
+    Friend WithEvents BtnEmailShare As Button
+    Friend WithEvents BtnPdfShare As Button
+    Friend WithEvents LblGraphData As Label
+    Friend WithEvents LblGraphType As Label
+    Friend WithEvents BtnBackToMenu As Button
 End Class
